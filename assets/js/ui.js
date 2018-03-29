9ccc14009bed1f3c604d19a4c3affb4629a725ed
Vahan Mnatsakanyan <vahan@mnaco.net>
2018-03-29 16:04:57 +0200

    Added main layout including aside, side navigation, tips

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,9 +1,24 @@
-// burger menu on moble layout
-(function() {
+// manage ui main functionality
+// including Bulma framework elements
+
+(function () {
+
+  // manage main menu 'burger menu' on moble layout
   var burger = document.querySelector('.burger');
   var menu = document.querySelector('#'+burger.dataset.target);
-  burger.addEventListener('click', function() {
+  burger.addEventListener('click', function () {
       burger.classList.toggle('is-active');
       menu.classList.toggle('is-active');
   });
+
+  // manage dropdowns
+  var dropdown = jQuery('.cc-dropdownmenu');
+  dropdown.on('click', function () {
+    event.stopPropagation();
+    jQuery(this).toggleClass('is-active');
+  });
+  jQuery('body').on('click', function () {
+    dropdown.removeClass('is-active');
+  });
+
 })();
