b3c124a04b94f85615b387a581a032bc8b194902
Vahan Mnatsakanyan <vahan@mnaco.net>
2018-04-04 20:27:01 +0200

    Fixed the product list layout. Added Anwendungsbereiche layout

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -5,10 +5,10 @@
 
   // manage main menu 'burger menu' on moble layout
   var burger = document.querySelector('.burger');
-  var menu = document.querySelector('#'+burger.dataset.target);
+  var menu = document.querySelector('#' + burger.dataset.target);
   burger.addEventListener('click', function () {
-      burger.classList.toggle('is-active');
-      menu.classList.toggle('is-active');
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
   });
 
   // manage dropdowns
@@ -22,54 +22,67 @@
   });
 
   // aside menu dropdown (available on mobiles)
-  jQuery('.cc-side-menu-dropdown').on('click', function() {
+  jQuery('.cc-side-menu-dropdown').on('click', function () {
     jQuery(this).toggleClass('is-active');
     jQuery(this).next('.cc-aside-menu').toggleClass('is-hidden-mobile');
   });
 
+  // ellipsis
+  jQuery('.cc-item-content .content').succinct({
+    size: '160',
+    omission: ' &rarr;'
+  });
+
+  // 'column-list' view trigger in products list
+  jQuery('.cc-show-list').on('click', function () {
+    jQuery('.cc-product-list').addClass('cc-view-list');
+  });
+  jQuery('.cc-show-column').on('click', function () {
+    jQuery('.cc-product-list').removeClass('cc-view-list');
+  });
+  
 })();
 
 
-(function(){
+// floating sticky header
+(function () {
 
   // Hide Header on on scroll down
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
-  var navbarHeight = jQuery('.cc-page-header').outerHeight();
+  var headerSelector = jQuery('.cc-page-header');
+  var navbarHeight = headerSelector.outerHeight();
 
-$(window).scroll(function(event){
+  jQuery(window).scroll(function (event) {
     didScroll = true;
-    console.log('scroll', navbarHeight);
-});
+  });
 
-setInterval(function() {
+  setInterval(function () {
     if (didScroll) {
-        hasScrolled();
-        didScroll = false;
+      hasScrolled();
+      didScroll = false;
     }
-}, 250);
+  }, 250);
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
+  function hasScrolled() {
+    var st = jQuery(this).scrollTop();
+
     // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.cc-page-header').removeClass('nav-down').addClass('nav-up');
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      headerSelector.removeClass('nav-down').addClass('nav-up');
     } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.cc-page-header').removeClass('nav-up').addClass('nav-down');
-        }
+      // Scroll Up
+      if (st + jQuery(window).height() < jQuery(document).height()) {
+        headerSelector.removeClass('nav-up').addClass('nav-down');
+      }
     }
-    
+
     lastScrollTop = st;
-}
+  }
 
 })();
